06501486196e2702c99dc4c2e3e0c4ff8683fc24
Khushi <khushi.gupta@w3villa.com>
2023-06-12 10:57:06 +0530

    Functioning calC

---
+++ index.js
@@ -0,0 +1,44 @@
+let currentOperator = '';
+let currentValue = '';
+let resultValue = '';
+
+function number(num) {
+    currentValue += num;
+    document.getElementById('result').value = currentValue;
+}
+
+function operator(operator) {
+    document.getElementById('result').value += operator;
+    currentOperator = operator;
+    resultValue = currentValue;
+    currentValue = '';
+}
+
+function calculate() {
+    let result = 0;
+    const num1 = parseFloat(resultValue);
+    console.log (resultValue)
+    console.log (currentValue)
+
+    const num2 = parseFloat(currentValue);
+
+    if (currentOperator === '+') {
+        result = num1 + num2;
+    } else if (currentOperator === '-') {
+        result = num1 - num2;
+    } else if (currentOperator === '*') {
+        result = num1 * num2;
+    } else if (currentOperator === '/') {
+        result = num1 / num2;
+    }
+
+    document.getElementById('result').value = result;
+    currentValue = result.toString();
+    currentOperator = '';
+    resultValue = '';
+}
+
+function clearResult() {
+    currentValue = '';
+    document.getElementById('result').value = '';
+}
