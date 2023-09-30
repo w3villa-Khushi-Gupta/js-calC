7c0c8572d9b27457d25dac15460846538eb42e2c
Khushi <khushi.gupta@w3villa.com>
2023-09-30 17:03:33 +0530

    30-sep-23

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-let currentOperator = '';
-let currentValue = '';
-let resultValue = '';
+        let currentOperator = '';
+        let currentValue = '';
+        let resultValue = '';
+        let op = 0;
 
-function number(num) {
-    currentValue += num;
-    document.getElementById('result').value = currentValue;
-}
+        function number(num) {
+            currentValue += num;
+            document.getElementById('result').value = currentValue;
+        }
 
-function operator(operator) {
-    document.getElementById('result').value += operator;
-    currentOperator = operator;
-    resultValue = currentValue;
-    currentValue = '';
-}
+        function operator(operator) {
+            document.getElementById('result').value += operator;
+            currentOperator = operator;
+            resultValue = currentValue;
+            currentValue = '';
+        }
 
-function calculate() {
-    let result = 0;
-    const num1 = parseFloat(resultValue);
-    console.log (resultValue)
-    console.log (currentValue)
+        function calculate() {
+            let result = 0;
+            const num1 = parseFloat(resultValue);
+            console.log (resultValue)
+            console.log (currentValue)
 
-    const num2 = parseFloat(currentValue);
+            const num2 = parseFloat(currentValue);
 
-    if (currentOperator === '+') {
-        result = num1 + num2;
-    } else if (currentOperator === '-') {
-        result = num1 - num2;
-    } else if (currentOperator === '*') {
-        result = num1 * num2;
-    } else if (currentOperator === '/') {
-        result = num1 / num2;
-    }
+            if (currentOperator === '+') {
+                result = num1 + num2;
+            } else if (currentOperator === '-') {
+                result = num1 - num2;
+            } else if (currentOperator === '*') {
+                result = num1 * num2;
+            } else if (currentOperator === '/') {
+                result = num1 / num2;
+            }
 
-    document.getElementById('result').value = result;
-    currentValue = result.toString();
-    currentOperator = '';
-    resultValue = '';
-}
+            document.getElementById('result').value = result;
+            currentValue = result.toString();
+            currentOperator = '';
+            resultValue = '';
+        }
 
-function clearResult() {
-    currentValue = '';
-    document.getElementById('result').value = '';
-}
+        function clearResult() {
+            currentValue = '';
+            document.getElementById('result').value = '';
+        }
